f56063a6200efc84a9d03172e4d49e6443e0ef73
Luis Majano <lmajano@gmail.com>
2015-08-13 11:13:02 -0500

    added new source code headers

--- src/java/luceecli/ServletConfigImpl.java
+++ src/java/luceecli/ServletConfigImpl.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2012 Ortus Solutions, Corp
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
 package luceecli;
 
 import java.util.Enumeration;
@@ -5,45 +21,49 @@ import java.util.Enumeration;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 
+public class ServletConfigImpl implements ServletConfig{
 
-public class ServletConfigImpl implements ServletConfig {
-
-	private String servletName;
-	private ServletContext context;
+	private final ServletContext	context;
+	private final String			servletName;
 
 	/**
 	 * Constructor of the class
+	 * 
 	 * @param parameters
 	 * @param attrs
 	 * @param servletName
 	 */
-	public ServletConfigImpl(CLIContext context,String servletName){
-		this.servletName=servletName;
-		this.context=context;
+	public ServletConfigImpl( CLIContext context, String servletName ){
+		this.servletName = servletName;
+		this.context = context;
 	}
-	
+
 	/**
 	 * @see javax.servlet.ServletConfig#getInitParameter(java.lang.String)
 	 */
-	public String getInitParameter(String key) {
-		return context.getInitParameter(key);
+	@Override
+	public String getInitParameter( String key ){
+		return this.context.getInitParameter( key );
 	}
 
 	/**
 	 * @see javax.servlet.ServletConfig#getInitParameterNames()
 	 */
-	public Enumeration<String> getInitParameterNames() {
-		return context.getInitParameterNames();
+	@Override
+	public Enumeration< String > getInitParameterNames(){
+		return this.context.getInitParameterNames();
+	}
+
+	@Override
+	public ServletContext getServletContext(){
+		return this.context;
 	}
 
 	/**
 	 * @see javax.servlet.ServletConfig#getServletName()
 	 */
-	public String getServletName() {
-		return servletName;
-	}
-
-	public ServletContext getServletContext() {
-		return context;
+	@Override
+	public String getServletName(){
+		return this.servletName;
 	}
 }
