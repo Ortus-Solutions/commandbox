d03d091fe62765b42c3e2bef1ea8f53488cecbcc
Jon Clausen <jon_clausen@silowebworks.com>
2016-02-16 11:11:28 -0500

    updates to hombrew template based on audit feedback

--- build/brew-template.rb
+++ build/brew-template.rb
@@ -1,32 +1,40 @@
 require "formula"
 # Version update PR to Homebrew requires only the information below:
-class Commandbox@be@ < Formula
-  desc "A CLI, package manager, app scaffolding tool with embedded CFML server"
+class Commandbox < Formula
+  desc "CFML embedded server, package manager, and app scaffolding tools"
   homepage "http://www.ortussolutions.com/products/commandbox"
   url "@repoURL@/ortussolutions/commandbox/@version@/commandbox-bin-@version@.zip"
-  sha256 "@sha256@"
-  
+  sha1 "cb8d454b99d56c16e8501b4e1aa3c4eeb9b44451"
+
   depends_on :arch => :x86_64
   depends_on :java => "1.7+"
 
-  resource 'apidocs' do
+  resource "apidocs" do
     url "@repoURL@/ortussolutions/commandbox/@version@/commandbox-apidocs-@version@.zip"
-    sha256 "@apidocs.sha256@"
+    sha1 "@apidocs.sha256@"
   end
 
   def install
-    bin.install 'box'
-    doc.install resource( "apidocs" )
+    bin.install "box"
+    doc.install resource("apidocs")
+  end
+
+  def caveats; <<-EOS.undent
+    CommandBox is licensed as open source software under the GNU Lesser General Public License v3
+
+    License information at:
+    http://www.gnu.org/licenses/lgpl-3.0.en.html
+
+    For full CommandBox documentation visit:
+    https://ortus.gitbooks.io/commandbox-documentation/
+
+    Source Code:
+    https://github.com/Ortus-Solutions/commandbox
+    EOS
   end
 
   test do
-    box install
-    box server start
+    system "box", "install"
+    system "box", "--version"
   end
-
-  def caveats
-    "You will need at least Java JDK 1.7+ to run CommandBox"
-  end
-
-
 end
