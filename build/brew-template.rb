1d3c609750e1a9ff8df0a04e47802841fac34ffa
Jon Clausen <jon_clausen@silowebworks.com>
2016-02-15 22:38:15 -0500

    update hombrew template and build for native formula compatibility

--- build/brew-template.rb
+++ build/brew-template.rb
@@ -1,21 +1,27 @@
 require "formula"
-
+# Version update PR to Homebrew requires only the information below:
 class Commandbox@be@ < Formula
+  desc "A CLI, package manager, app scaffolding tool with embedded CFML server"
   homepage "http://www.ortussolutions.com/products/commandbox"
   url "@repoURL@/ortussolutions/commandbox/@version@/commandbox-bin-@version@.zip"
-  sha1 "@sha1@"
-  version "@version@"
+  sha256 "@sha256@"
   
   depends_on :arch => :x86_64
+  depends_on :java => "1.7+"
 
   resource 'apidocs' do
     url "@repoURL@/ortussolutions/commandbox/@version@/commandbox-apidocs-@version@.zip"
-    sha1 "@apidocs.sha1@"
+    sha256 "@apidocs.sha256@"
   end
 
   def install
     bin.install 'box'
     doc.install resource( "apidocs" )
+  end
+
+  test do
+    box install
+    box server start
   end
 
   def caveats
